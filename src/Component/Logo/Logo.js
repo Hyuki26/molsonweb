e898e5cdc5eda6dddc9108d88c04a101838b31b8
Yuuri Harayama <hyuki@icstars.org>
2021-03-30 22:52:24 -0500

    hi

---
+++ src/Component/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import mcbc from '../../Pics/mcbcd.jpg';
+
+function Logo (props){
+    return(
+        
+        <div>
+            <img src={mcbc} className='hello' alt="pics"/>
+        </div>
+    )
+};
+
+export default Logo;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+import Logo from '../../Component/Logo/Logo';
+
+function Home (){
+    return(
+        <div>  
+          <Logo/>
+        </div>
+    )
+};
+
+export default Home;
+
