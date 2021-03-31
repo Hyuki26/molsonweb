aa5599e148c33c25ac8b09b9c0d118cce35e602e
Yuuri Harayama <hyuki@icstars.org>
2021-03-30 23:47:34 -0500

    hi

--- src/Component/Navbar/Nav.js
+++ src/Component/Navbar/Nav.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
-  }, y
+  }, 
   title: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
